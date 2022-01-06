fedbda2579203c49cc6b8ad16cf36fa7a9cff016
¨Venturin¨ <diegoventurinn@gmail.com>
2022-01-06 17:41:00 -0300

    Adicionado requisito 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+
+  const verify = (person) => person.firstName === employeeName || person.lastName === employeeName;
+  const employee = employees.find(verify);
+  const result = (employee !== undefined) ? employee : {};
+
+  return result;
 }
 
 module.exports = getEmployeeByName;
+
+// console.log(getEmployeeByName('Bethea'));
