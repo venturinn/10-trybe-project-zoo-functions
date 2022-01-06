88599e667fed356abcc7c9b081717ba90ba20494
¨Venturin¨ <diegoventurinn@gmail.com>
2022-01-06 17:13:35 -0300

    Adicionado requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+
+  const animalUser = data.species.find((element) => element.name === animal);
+  return animalUser.residents.every((element) => element.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
+
+// console.log(getAnimalsOlderThan('bears', 4));
