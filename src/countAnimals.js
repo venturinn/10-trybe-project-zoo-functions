7bb074749cfcf6d0aff090034d90aca90668f915
¨Venturin¨ <diegoventurinn@gmail.com>
2022-01-07 09:17:20 -0300

    Adicionado requisito 05 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,31 +1,51 @@
 const data = require('../data/zoo_data');
 
+function allAnimals() {
+  const getAnimals = (animalName) => ({ [animalName.name]: animalName.residents.length });
+  const animalsName = data.species.map(getAnimals);
+
+  const result = {};
+  animalsName.forEach((element) => Object.assign(result, element));
+
+  return result;
+}
+
+function animalsWithoutSex(animal) {
+  const getPopularity = (animalName) => animal.specie === animalName.name;
+  const population = data.species.filter(getPopularity);
+  return population[0].residents.length;
+}
+
+function animalsWithSex(animal) {
+  let totalCount = 0;
+  const getPopularity = (animalName) => animal.specie === animalName.name;
+  const population = data.species.filter(getPopularity);
+
+  const count = (individual) => {
+    if (individual.sex === animal.sex) {
+      totalCount += 1;
+    }
+  };
+  population[0].residents.forEach(count);
+  return totalCount;
+}
+
 function countAnimals(animal) {
   // seu código aqui
   if (animal === undefined) {
-
-   let total = {}
-
-    const getPopularity = (animalName) => (animalName.popularity);
-    const popularity = data.species.map(getPopularity);
-    const [aa, bb, cc, dd, ee, ff, gg, hh, ii] = popularity;
-
-    const getAnimals = (animalName) => ({
-
-     a:sanimalName.popularity
-
-     });
-    const animalsName = data.species.map(getAnimals);
-    
-    return animalsName;
+    return allAnimals();
   }
   if (Object.keys(animal).length === 1) {
-    const getPopularity = (animalName) => animal.specie === animalName.name;
-    const popularity = data.species.filter(getPopularity);
-    return popularity[0].popularity;
+    return animalsWithoutSex(animal);
+  }
+  if (Object.keys(animal).length === 2) {
+    return animalsWithSex(animal);
   }
 }
 
 module.exports = countAnimals;
 
 console.log(countAnimals());
+console.log(countAnimals({ specie: 'giraffes'}));
+console.log(countAnimals({ specie: 'giraffes', sex: 'male' }));
+
