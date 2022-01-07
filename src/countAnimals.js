ace9789472c1c3ca4efe1706cec4cd1b11fd230a
¨Venturin¨ <diegoventurinn@gmail.com>
2022-01-06 22:28:13 -0300

    Adiconada solução parcial do requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,30 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+
+   let total = {}
+
+    const getPopularity = (animalName) => (animalName.popularity);
+    const popularity = data.species.map(getPopularity);
+    const [aa, bb, cc, dd, ee, ff, gg, hh, ii] = popularity;
+
+    const getAnimals = (animalName) => ({
+
+     a:sanimalName.popularity
+
+     });
+    const animalsName = data.species.map(getAnimals);
+    
+    return animalsName;
+  }
+  if (Object.keys(animal).length === 1) {
+    const getPopularity = (animalName) => animal.specie === animalName.name;
+    const popularity = data.species.filter(getPopularity);
+    return popularity[0].popularity;
+  }
 }
 
 module.exports = countAnimals;
+
+console.log(countAnimals());
